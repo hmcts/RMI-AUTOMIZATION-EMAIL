f2dfbf2d4c325051242b9a622b0dfc96f9243143
Irfan Siddique Siddique <irfan.siddique@hmcts.net>
2018-07-30 14:59:47 +0100

    Data Access Layer Changes

--- src/main/java/uk/gov/hmcts/reform/rmi/dao/EmailAutomizationDAO.java
+++ src/main/java/uk/gov/hmcts/reform/rmi/dao/EmailAutomizationDAO.java
@@ -74,9 +74,6 @@ public class EmailAutomizationDAO implements EmailAutomizationDAOImpl {
 			
 		  });
 		  logger.info("EmailAutomizationDAO::cardPaymentFileProcessor::End"); 
-		}
-	
-	
-	
+		}	
 	
 }
