52fbf0303367dd25668a9cab82e04ec9b9fcf681
Irfan Siddique Siddique <irfan.siddique@hmcts.net>
2018-07-30 09:43:06 +0100

    Changed the Schedular

--- src/main/java/uk/gov/hmcts/reform/rmi/EmailAttachmentAutomizationApp.java
+++ src/main/java/uk/gov/hmcts/reform/rmi/EmailAttachmentAutomizationApp.java
@@ -5,29 +5,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import uk.gov.hmcts.reform.rmi.properties.ApplicationProperties;
 
 @EnableScheduling
 @EnableAutoConfiguration
-@SpringBootApplication(scanBasePackages={"uk.gov.hmcts.reform.rmi.emailattachment"})
-public class EmailAttachmentAutomizationApp implements CommandLineRunner {
-
-	
-	@Autowired
-	private  ApplicationProperties applicationProperties;
+@SpringBootApplication(scanBasePackages={"uk.gov.hmcts.reform.rmi"})
+public class EmailAttachmentAutomizationApp {
 	
 	
-	public static void main(String[] args) {	
-		
+	public static void main(String[] args) {			
 		SpringApplication.run(EmailAttachmentAutomizationApp.class);	
 	}
-
-	@Override
-	public void run(String... arg0) throws Exception {
-		System.out.println("The property is: " + applicationProperties.getAccountKey() + ":::::::::::::::::::::::::::;");
-
-		
-	}
+	
+	@Bean
+	 public TaskScheduler ScheduledTaskService(){
+	    return new ThreadPoolTaskScheduler();
+	 }
 }
